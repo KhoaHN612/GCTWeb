3970178544af613363e087af42f035578cf49bb1
PHP <phphong211@gmail.com>
2025-06-05 16:01:51 +0700

    Add Filter and Search Function in Product Page

---
+++ Models/ViewModels/ProductCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GCTWeb.Models.ViewModels;
+
+public class ProductCategoryViewModel {
+    public IEnumerable<Product> Products { get; set; }
+    public IEnumerable<Category> Categories { get; set; }
+    public IEnumerable<Brand> Brands { get; set; }
+    public IEnumerable<Grade> Grades { get; set; }
+    
+    public int? SelectedCategoryId { get; set; }
+    public string SearchTerm { get; set; }
+    public string SortOption { get; set; }
+    public string BrandOption { get; set; }
+    public string GradeOption { get; set; }
+}
