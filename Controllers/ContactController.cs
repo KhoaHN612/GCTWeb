ccef81a941ae1a97591bc57194c3bbceb0637abb
KhoaGCC210023 <khoahnvithuy@gmail.com>
2025-09-22 21:11:39 +0700

    [ADD] Add contact and introduction page

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCTWeb.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: ContactController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/IntroductionController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCTWeb.Controllers
+{
+    public class IntroductionController : Controller
+    {
+        // GET: IntroductionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
