4e21fe2e9dd23d22df48c1045a0216b20b30f97f
KhoaGCC210023 <khoahnvithuy@gmail.com>
2025-10-03 21:19:39 +0700

    [ADD] Add notification for admin

---
+++ ViewComponents/OrderNotificationsViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using GCTWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCTWeb.ViewComponents
+{
+    public class OrderNotificationsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderNotificationsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pendingOrders = await _context.Orders
+                .Where(o => o.Status == Models.Enums.OrderStatus.Pending)
+                .OrderByDescending(o => o.CreatedAt)
+                .ToListAsync();
+
+            var totalPending = pendingOrders.Count;
+
+            ViewBag.TotalPending = totalPending;
+
+            return View(pendingOrders);
+        }
+    }
+}
